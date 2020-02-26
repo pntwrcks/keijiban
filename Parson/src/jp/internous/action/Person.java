960ecbcf57561e4d5d7f2bb926103b5277934930
pntwrcks <remy-bonjasky@ezweb.ne.jp>
2020-02-26 17:52:58 +0900

    コメント

---
+++ Parson/src/jp/internous/action/Person.java
@@ -0,0 +1,9 @@
+package jp.internous.action;
+
+public class Person {
+	public String name = null;
+	public int age = 0;
+	public String phoneNunber=null;
+	public String address=null;
+
+}
---
+++ Parson/src/jp/internous/action/Test.java
@@ -0,0 +1,41 @@
+package jp.internous.action;
+
+public class Test{
+	public static void main(String[]args) {
+		Person taro = new Person();
+		taro.name=" 山田太郎" ;
+		taro.age=20;
+
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+
+		Person zirou = new Person();
+		zirou.name=" 木村次郎" ;
+		zirou.age=18;
+
+		System.out.println(zirou.name);
+		System.out.println(zirou.age);
+
+		Person hanako = new Person();
+		hanako.name=" 鈴木花子" ;
+		hanako.age=16;
+
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
+
+		Person syu = new Person();
+		syu.name=" 市川周" ;
+		syu.age=24;
+
+		System.out.println(syu.name);
+		System.out.println(syu.age);
+
+
+
+	}
+
+
+}
+
+
+
